8ca540646e04548af0842c581d754ab82b31699d
bug320 <1046218884@qq.com>
2017-05-05 08:22:06 +0800

    success

---
+++ main.py
@@ -0,0 +1,18 @@
+﻿# -*- coding:utf-8 -*-
+import web
+
+urls = (
+"/","index"
+)
+
+class index:
+	def GET(self):
+		render=web.template.render("templates")
+		return render.index("CBC")
+		
+if __name__ == "__main__":
+	app = web.application(urls,globals())
+	app.run()
+	
+	
+		
